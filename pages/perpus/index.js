25eeba9e7d679f2136b6790b8cc18b26b45a21f7
joelelo <elioenaijoel@gmail.com>
2022-07-05 20:22:02 +0700

    "Library searchbar"

--- pages/perpus/index.js
+++ pages/perpus/index.js
@@ -2,19 +2,22 @@ import React from 'react';
 import Navbar from '../../components/navbar';
 import Image from 'next/image';
 import styles from './library.module.scss';
-
+import { Input } from 'antd';
+const { Search } = Input;
 const Repository = () => {
+    const handleSearch = (value) => {
+        console.log(value);
+    };
     return (
         <div>
             <Navbar />
             <div className={styles.imagecontainer}>
                 <div className={styles.tbox}>
-                    <h1 className={styles.inimage}>Perpustakaan</h1>
+                    <h1 className={styles.inimage}>PERPUSTAKAAN</h1>
                 </div>
-
-                <Image src={'/images/library.jpg'} width={500} height={250} layout="responsive" />
+                <Image src={'/images/library.jpg'} width={500} height={250} layout="responsive" priority="true" />
             </div>
-            Repository
+            <Search placeholder="Cari Judul atau Penulis" onSearch={handleSearch} style={{ width: '20%', marginLeft: '40%', marginTop: '2rem' }} />
         </div>
     );
 };
