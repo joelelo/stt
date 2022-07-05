795365554ee85cc476a48f23dfec910074241ed5
joelelo <elioenaijoel@gmail.com>
2022-07-05 17:44:23 +0700

    "Add sass and image for library"

--- pages/perpus/index.js
+++ pages/perpus/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Navbar from '../../components/navbar';
+import Image from 'next/image';
+import styles from './library.module.scss';
 
 const Repository = () => {
     return (
         <div>
             <Navbar />
+            <div className={styles.imagecontainer}>
+                <div className={styles.tbox}>
+                    <h1 className={styles.inimage}>Perpustakaan</h1>
+                </div>
+
+                <Image src={'/images/library.jpg'} width={500} height={250} layout="responsive" />
+            </div>
             Repository
         </div>
     );
