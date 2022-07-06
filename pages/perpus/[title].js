73c82f48eb62fbffde4bd53f83252d91d7f290aa
joelelo <elioenaijoel@gmail.com>
2022-07-06 11:34:52 +0700

    "Preview & Download"

--- pages/perpus/[title].js
+++ pages/perpus/[title].js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { Document } from 'react-pdf';
 
 const Library = () => {
     return (
         <div>
             <object data="/api/ebook"></object>
+            <iframe
+                src="https://drive.google.com/file/d/0B7SDIyL-UUAESnJYcGZha2MwMnc/preview?resourcekey=0-P-_UKqhq5DQN_JqRH8ee4g"
+                width="640"
+                height="480"
+                allow="autoplay"></iframe>
+            <a href="https://drive.google.com/uc?export=download&id=0B7SDIyL-UUAESnJYcGZha2MwMnc">Download</a>
         </div>
     );
 };
