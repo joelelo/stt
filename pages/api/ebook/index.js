47d8293fac901158e783b39a5a3f10b120a1bd99
joelelo <elioenaijoel@gmail.com>
2022-07-06 11:18:15 +0700

    "pdf backbone"

---
+++ pages/api/ebook/index.js
@@ -0,0 +1,37 @@
+import fs from 'fs';
+
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
+
+const CHUNK_SIZE_IN_BYTES = 100000;
+export default function handler(req, res) {
+    const { method } = req;
+    switch (method) {
+        case 'GET':
+            //read file use '/api/ebook' at src to use
+            const range = req.headers.range;
+            console.log('Range: ' + range);
+            if (!range) {
+                return res.status(400).send('range must be provided');
+            }
+
+            const filestream = fs.createReadStream('./ebooks/Building.pdf');
+            const fileSize = fs.statSync('./ebooks/Building.pdf').size;
+
+            const headers = {
+                'Content-Length': fileSize,
+                'Content-Type': 'application/pdf',
+            };
+            res.writeHead(206, headers);
+            filestream.pipe(res);
+            break;
+        case 'POST':
+            res.status(200).json({ message: 'success!' });
+            break;
+        default:
+            res.status(405).json({ error: `Method ${method} not allowed` });
+    }
+}
---
+++ pages/perpus/[title].js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Document } from 'react-pdf';
+
+const Library = () => {
+    return (
+        <div>
+            <object data="/api/ebook"></object>
+        </div>
+    );
+};
+
+export default Library;
